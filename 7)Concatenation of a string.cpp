854e9f0d98932fcbf9d88481024edc7a28122011
Ishanshah20 <ishanshahishanshah14@gmail.com>
2024-10-23 15:28:44 +0530

    Create 7)Concatenation of a string.cpp

---
+++ 7)Concatenation of a string.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Dhanraj";
+    last_name = "Chavan";
+
+    cout<<first_name+last_name;
+}
+
+/*
+DhanrajChavan
+*/
